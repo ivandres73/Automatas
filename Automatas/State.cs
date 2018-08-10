03a2ed1bb555a41dea80a50d577ce5fd1d632127
Ivan Carcamo <ivandres73@unitec.edu>
2018-08-10 03:21:37 -0600

    States Diagram classes added, not tested yet

---
+++ Automatas/State.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automatas
+{
+    struct arista
+    {
+        public char entrada;
+        public State nextState;
+
+        public arista(char c, State s)
+        {
+            entrada = c;
+            nextState = s;
+        }
+    }
+    class State
+    {
+        public int num { get; set; }
+        public List<arista> aristas;
+        public bool isFinal { get; set; }
+
+        public State()
+        {
+            num = 0;
+            aristas = null;
+            isFinal = false;
+        }
+
+        public State(int n, bool f)
+        {
+            num = n;
+            aristas = new List<arista>();
+            isFinal = f;
+        }
+
+        public void addEntrada(char ent, State sig)
+        {
+            arista nueva = new arista(ent, sig);
+            aristas.Add(nueva);
+        }
+
+        public void printAristas()
+        {
+            Console.WriteLine("Las aristas de q" + num + "son:");
+            foreach(arista a in aristas)
+            {
+                Console.WriteLine(a.entrada + "-> q" + a.nextState.num);
+            }
+        }
+    }
+}
---
+++ Automatas/StatesDiagram.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automatas
+{
+    class StatesDiagram
+    {
+        List<State> nuevos;
+
+        public StatesDiagram()
+        {
+            ;
+        }
+
+        public void createStates(Automata a)
+        {
+            nuevos = new List<State>();
+            State nuevo;
+            for (byte i=0; i < a.Q; i++)
+            {
+                nuevo = new State(i, false);
+                nuevos.Add(nuevo);
+            }
+
+            for (byte i=0; i < a.Q; i++)
+            {
+                for(byte v=1; v <= a.Sigma.Length; v++)
+                {
+                    int sigEstado = a.Delta[i, v];
+                    char entrada = a.Sigma[v-1];
+                    nuevos[i].addEntrada(entrada, nuevos[sigEstado]);
+                }
+            }
+
+            foreach (int i in a.FState)
+            {
+                nuevos[i].isFinal = true;
+            }
+        }
+
+        public void printDiagram()
+        {
+            if (nuevos == null)
+            {
+                Console.WriteLine("diagrama vacio...");
+                return;
+            }
+            
+            foreach(State s in nuevos)
+            {
+                s.printAristas();
+            }
+        }
+    }
+}
